9bc96c6fba00da384681e4c4daeb2b0a5213e2eb
playpis <benzyfor@gmail.com>
2026-04-23 10:29:07 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.static("./"));
 
 let cache = {
   btc: "--",
+  btc_24h: 0,
   gold: "--",
+  gold_24h: 0,
   time: 0
 };
 
-// 缓存 60 秒
 const CACHE_TTL = 60 * 1000;
 
 app.get("/api/price", async (req, res) => {
@@ -22,21 +22,25 @@ app.get("/api/price", async (req, res) => {
 
   try {
     const r = await fetch(
-      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,tether-gold&vs_currencies=usd"
+      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,tether-gold&vs_currencies=usd&include_24hr_change=true"
     );
 
     const data = await r.json();
 
     cache = {
       btc: data.bitcoin.usd,
+      btc_24h: data.bitcoin.usd_24h_change?.toFixed(2) ?? 0,
+
       gold: data["tether-gold"].usd,
+      gold_24h: data["tether-gold"].usd_24h_change?.toFixed(2) ?? 0,
+
       time: now
     };
 
     res.json(cache);
 
   } catch (e) {
-    res.json(cache); // 出错返回旧数据
+    res.json(cache);
   }
 });
 
